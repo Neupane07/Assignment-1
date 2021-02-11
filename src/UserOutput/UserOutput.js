d5277d87cc350176619d7f7060d1f6d85beec357
Neupane07 <hbkbimal07@gmail.com>
2021-02-11 19:24:17 +0530

    added components

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserOutput = () => {
+    return (
+        <div>
+            <p>Some random text!</p>
+            <p>Text to get overwritten</p>
+        </div>
+    );
+};
+
+export default UserOutput;
